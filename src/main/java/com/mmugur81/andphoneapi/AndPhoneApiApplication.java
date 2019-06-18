db6e751609d746edb3122a0dc01f9b6394ca8755
Mugurel Mirica <mmugur81@gmail.com>
2019-06-18 21:48:21 +0300

    Init framework

---
+++ src/main/java/com/mmugur81/andphoneapi/AndPhoneApiApplication.java
@@ -0,0 +1,13 @@
+package com.mmugur81.andphoneapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AndPhoneApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AndPhoneApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mmugur81/andphoneapi/AndPhoneApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mmugur81.andphoneapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AndPhoneApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
